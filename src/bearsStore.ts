3f005d9e3fedd0206d671a4fb0943987b160fede
parrot <strongmoon@protonmail.com>
2022-10-28 19:40:31 +0200

    wip: create setter

--- src/bearsStore.ts
+++ src/bearsStore.ts
@@ -1,8 +1,6 @@
-import { create } from "./lib/index"
-
+import { create } from "./lib/index";
 
 export const useBearStore = create((set) => ({
-    bears: 0,
-    increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-    removeAllBears: () => set({ bears: 0 }),
-  }))
+  bears: 0,
+  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+}));
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,31 @@
-export const create = () => {}
+import { useState } from "react";
+
+export const create = <TState extends Record<string, unknown>>(
+  stateCreator: (
+    set: (reducer: (prevState: TState) => Partial<TState>) => Partial<TState>
+  ) => TState
+) => {
+  let globalState: TState;
+
+  const set = (
+    reducer: (prevState: TState) => Partial<TState>
+  ): Partial<TState> => {
+    const newPartialState = reducer(globalState);
+
+    globalState = { ...globalState, ...newPartialState };
+
+    return globalState;
+  };
+
+  globalState = stateCreator(set);
+
+  const useStore = (
+    selector: (state: TState | undefined) => keyof TState | undefined
+  ) => {
+    const [state, setState] = useState();
+
+    return selector(state);
+  };
+
+  return useStore;
+};
